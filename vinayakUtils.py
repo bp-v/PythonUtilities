063dc468484c3a32a63565fc2193bbb530cd5358
vinayakPatel <vinayak.chronicles@gmail.com>
2020-10-25 23:23:43 +0530

    Update vinayakUtils.py

--- vinayakUtils.py
+++ vinayakUtils.py
@@ -1,4 +1,7 @@
 import os
+import json
+import io
+
 
 # Directory of the script being run
 directory_of_script = os.path.dirname(os.path.abspath(__file__))
@@ -11,4 +14,26 @@ current_working_directory = os.path.abspath(os.getcwd())
 
 print("current_working_directory: ", current_working_directory)
 
+
+def loadFile(filename):
+  text = ""
+  with io.open(filename, 'r', encoding='utf8') as f:
+    text = f.read()
+  return text
+
+
+def saveFile(filename, text):
+  with io.open(filename, 'w', encoding='utf8') as f:
+    f.write(text)
+    
+    
+def saveJsonToFile(filename, data_dict):
+  # data is generally a dict
+  text = json.dumps(data_dict, sort_keys=False, indent=4)
+  saveFile(filename, text)
+  
+  
+def loadJsonToFile(filename):
+  text = loadFile(filename)
+  return json.loads(text)
 
