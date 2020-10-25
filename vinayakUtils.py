06eac83d962cb98ba73d884a43590396a9e2da01
vinayakPatel <vinayak.chronicles@gmail.com>
2020-10-25 23:29:05 +0530

    Update vinayakUtils.py

--- vinayakUtils.py
+++ vinayakUtils.py
@@ -1,18 +1,18 @@
 import os
-import json
 import io
+import json
 
 
-# Directory of the script being run
-directory_of_script = os.path.dirname(os.path.abspath(__file__))
+def get_script_directory():
+  # Directory of the script being run
+  # directory_of_script = os.path.dirname(os.path.abspath(__file__))
+  return os.path.dirname(os.path.abspath(__file__))
 
-print("directory_of_script: ", directory_of_script)
 
-
-# Current working directory
-current_working_directory = os.path.abspath(os.getcwd())
-
-print("current_working_directory: ", current_working_directory)
+def get_current_working_directory():
+  # Current working directory
+  # current_working_directory = os.path.abspath(os.getcwd())
+  return os.path.abspath(os.getcwd())
 
 
 def loadFile(filename):
